f244031a1cd1016307064d8dba2b98feb47f0d83
Tarjei Husøy <tarjei@roms.no>
2014-03-25 20:48:16 +0100

    Packageify the module

--- pwm_server/__init__.py
+++ pwm_server/__init__.py
@@ -1,6 +1,6 @@
-import argparse
 from flask import Flask, request, jsonify, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+import argparse
 import os
 import hashlib
 import sys
@@ -10,33 +10,10 @@ app.config.from_envvar('PWM_CONFIG_FILE')
 db = SQLAlchemy(app)
 
 
-class DomainPassword(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    domain = db.Column(db.String(30))
-    salt = db.Column(db.String(128))
-
-
-    def __init__(self, **kwargs):
-        super(DomainPassword, self).__init__(**kwargs)
-        self.new_salt()
-
-
-    def new_salt(self):
-        self.salt = os.urandom(32).encode('base64')
-
-
-    def derive_domain_key(self, master_password):
-        key_bytes = hashlib.sha1('%s:%s' % (master_password, self.domain)).digest()
-        return key_bytes.encode('hex')
-
-
-    def __repr__(self):
-        return 'DomainPassword(domain=%s, dblt=%s)' % (self.domain, self.dblt)
-
-
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
+
 
 @app.route('/get')
 def main():
