85fbcb4078673fd2eb142fcbd9c689326e4829b2
Tarjei Husøy <tarjei@roms.no>
2014-04-13 23:56:26 +0200

    Add another csr test case

--- integration_tests/test_csr_process.py
+++ integration_tests/test_csr_process.py
@@ -13,3 +13,10 @@ class CSRTEst(unittest.TestCase):
     def test_welcome(self):
         response = requests.get(self.test_uri, verify=self.cert)
         self.assertEqual(response.status_code, 200)
+
+
+    def test_csr(self):
+        csr = open(os.path.join(os.path.dirname(__file__), 'test.csr'))
+        files = {'csr': csr}
+        response = requests.post(self.test_uri + '/ca/csr', verify=self.cert, files=files)
+        self.assertEqual(response.status_code, 202)
