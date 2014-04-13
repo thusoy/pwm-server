4d1dc36e7426a13906dd1b75eda2c8bff94c88b4
Tarjei Husøy <tarjei@roms.no>
2014-04-14 00:07:14 +0200

    Resolve config from envvar relative to cwd

--- pwm_server/__init__.py
+++ pwm_server/__init__.py
@@ -23,10 +23,10 @@ def create_app(config_file=None):
     if config_file:
         config_path = os.path.join(os.getcwd(), config_file)
         _logger.debug('Loading config from %s', config_path)
-        app.config.from_pyfile(config_path)
     else:
         _logger.debug('Loading config from envvar, file %s', os.environ['PWM_SERVER_CONFIG_FILE'])
-        app.config.from_envvar('PWM_SERVER_CONFIG_FILE')
+        config_path = os.path.join(os.getcwd(), os.environ['PWM_SERVER_CONFIG_FILE'])
+    app.config.from_pyfile(config_path)
 
     from . import views
     app.register_blueprint(views.mod)
